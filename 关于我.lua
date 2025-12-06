0decc135021698c536c4ad92ca2f27f4d040e424
HKJQ3 <2052346558@qq.com>
2025-12-06 20:00:40 +0800

    Update 关于我.lua

--- 关于我.lua
+++ 关于我.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HKJQ3/me/refs/heads/main/Protected_2233462745112743.lua"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8dbfa1627ec6c661d8c171711d9f699eca06a4fc25e19f4d38f3a0ee0eac014c/download", true))()
